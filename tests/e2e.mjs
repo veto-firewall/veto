7a4505ef932629ad55de07a704774896ceb0406a
Ruslan Bay <67730802+ruslanbay@users.noreply.github.com>
2025-08-05 16:37:39 +0000

    update tests

--- tests/e2e.mjs
+++ tests/e2e.mjs
@@ -1,35 +1,35 @@
-import fs from 'fs';
+import fs from 'node:fs';
+import path from 'node:path';
 import puppeteer from 'puppeteer';
-import path from 'path';
 
-const extensionPath = path.join(process.cwd(), 'dist/veto.zip');
-const firefoxProfilePath = 'tests/profile';
+const distribPath = path.join(process.cwd(), 'dist');
+
+const EXT_INTERNAL_UUID = crypto.randomUUID();
+const EXT_ID = JSON.parse(
+	fs.readFileSync(`${distribPath}/manifest.json`)
+).browser_specific_settings.gecko.id;
 
 const firefoxOptions = {
   browser: 'firefox',
   headless: true,
-  executablePath: '/home/codespace/.cache/puppeteer/firefox/linux-stable_141.0/firefox/firefox',
-  // enableExtensions: extensionPath,
-  args: [
-    '-profile', firefoxProfilePath,
-  ],
+  // executablePath: '/home/codespace/.cache/puppeteer/firefox/linux-stable_141.0.2/firefox/firefox',
+  extraPrefsFirefox: {
+    'extensions.webextensions.uuids': `{"${EXT_ID}": "${EXT_INTERNAL_UUID}"}`,
+  },
+  dumpio: false,
 };
 
 (async () => {
   const browser = await puppeteer.launch(firefoxOptions);
-  const extensionId = await browser.installExtension(extensionPath);
 
-  const extensionsJsonPath = `${firefoxProfilePath}/extensions.json`;
-  const content = fs.readFileSync(extensionsJsonPath, 'utf-8');
-  const extensionsData = JSON.parse(content);
-  const addons = extensionsData.addons || [];
-  const addon = addons.find(addon => addon.id === extensionId);
-  if (addon) {
-    console.log(`Found extension with id ${extensionId}:`);
-    console.log(addon);
-  } else {
-    console.log(`Extension with id ${extensionId} not found in extensions.json`);
-  }
+  await browser.installExtension(`${distribPath}/veto.zip`);
+  
+  const page = await browser.newPage();
+  // await page.goto(`https://9.9.9.9/dns`);
+  await page.goto(`moz-extension://${EXT_INTERNAL_UUID}/manifest.json`);
+  await page.screenshot({path: 'screenshot.png', fullPage: true});
+
+  await browser.uninstallExtension(EXT_ID);
 
   await browser.close();
 })();
